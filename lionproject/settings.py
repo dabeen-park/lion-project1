af6bace20b3a9a6f9dcef0c9ae02cde284a53446
dabeen-park <ysbeen00@gmail.com>
2021-07-24 22:31:36 +0900

    for heroku

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -147,8 +147,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #AWS
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID','AKIA3ZPXSVEP4EL74BMH')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY','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')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'likelion-django-lesson4'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
